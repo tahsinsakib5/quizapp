711213da064b9e2b2501e001568adec1b9a5f727
tahsinsakib5 <sakibulislam5162@gmail.com>
2023-10-22 09:11:51 -0700

    quizapp

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -8,29 +8,27 @@ class Helpers {
     List<Quiz> quizList = [];
 
     var collectionSnapshot =
-        await FirebaseFirestore.instance.collection('all_data').get();
+        await FirebaseFirestore.instance.collection('all_quiz').get();
 
     //Loop for All Document in Collection
     for (var documentSnapshot in collectionSnapshot.docs) {
-      String question = documentSnapshot.get('qution');
-      List answerMapList = documentSnapshot.get('answerlist');
+      String question = documentSnapshot.get('question');
+      List answerMapList = documentSnapshot.get('answerList');
 
-       var docID = documentSnapshot.reference.id;
+      var docID = documentSnapshot.reference.id;
 
-      print('Doc ID $docID');
+      // print('Doc ID $docID');
 
       List<Answer> answerList = [];
 
       //Loop for All Answers in Answer list in Document.///////
       for (var answerMap in answerMapList) {
         Answer myAnswer = Answer(
-            answer: answerMap['answer'], isCorrect: answerMap['iscorect']);
+            answer: answerMap['answer'], isCorrect: answerMap['isCorrect']);
         answerList.add(myAnswer);
-
-        
       }
 
-      Quiz myQuiz = Quiz(question: question, answerList: answerList,);
+      Quiz myQuiz = Quiz(question: question, answerList: answerList, id: docID);
 
       quizList.add(myQuiz);
 
